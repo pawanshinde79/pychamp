f260c88baf72fc5e16703c2f1ad2b00d0deed74c
Pawan <pawan@allygs.com>
2017-08-02 21:58:08 +0530

    Jyupiter Notebook

---
+++ lambda-anonymus.py
@@ -0,0 +1,11 @@
+# number_list = range(-5, 5)
+# less_than_zero = list(filter(lambda x:x < 0, number_list))
+# print less_than_zero
+
+
+# Print squared of each no from list
+elements = [1, 2, 3, 4, 5, 10]
+squared = []
+for i in elements:
+    squared.append(i**2)
+print squared
